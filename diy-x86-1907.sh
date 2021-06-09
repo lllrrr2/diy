f74c21790ba0444542a34cbbe6ef2d5f4b814192
hong0980 <hong0980@qq.com>
2021-06-09 15:19:12 +0800

    update@2021-06-09-15-19

--- diy-x86-1907.sh
+++ diy-x86-1907.sh
@@ -412,6 +412,9 @@ rm -rf package/diy/parted
 rm -rf package/diy/OpenAppFilter
 rm -rf diy/hong0980/autocore
 # rm -rf package/diy/luci-app-dockerman && sed -i 's/docker-ce/dockerd/' package/ipk/luci-app-dockerman/Makefile
+rm -rf feeds/packages/utils/dockerd && rm -rf feeds/packages/utils/docker && \
+svn co https://github.com/openwrt/packages/trunk/utils/dockerd feeds/packages/utils/dockerd && \
+svn co https://github.com/openwrt/packages/trunk/utils/docker feeds/packages/utils/docker
 rm -rf package/ipk/luci-app-dockerman && rm -rf package/diy/luci-app-dockerman && \
 git clone https://github.com/lisaac/luci-app-dockerman package/diy/luci-app-dockerman
 # rm -rf package/diy/luci-lib-docker
