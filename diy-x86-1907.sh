4d0ba17ea3bbebbfce2fdfd630c256a7ae699f03
hong0980 <hong0980@qq.com>
2021-07-07 18:29:20 +0800

    update@2021-07-07-18-29

--- diy-x86-1907.sh
+++ diy-x86-1907.sh
@@ -410,14 +410,17 @@ rm -rf package/diy/luci-app-diskman
 rm -rf package/diy/parted
 rm -rf package/diy/OpenAppFilter
 rm -rf diy/hong0980/autocore
-rm -rf package/diy/luci-app-dockerman #&& sed -i 's/docker-ce/dockerd/' package/ipk/luci-app-dockerman/Makefile
+rm -rf package/diy/luci-app-dockerman
 rm -rf feeds/packages/utils/dockerd && rm -rf feeds/packages/utils/docker && \
 svn co https://github.com/openwrt/packages/trunk/utils/dockerd feeds/packages/utils/dockerd && \
 svn co https://github.com/openwrt/packages/trunk/utils/docker feeds/packages/utils/docker
-# rm -rf package/ipk/luci-app-dockerman && rm -rf package/diy/luci-app-dockerman && \
-# git clone https://github.com/lisaac/luci-app-dockerman package/diy/luci-app-dockerman
-rm -rf package/diy/luci-lib-docker && git clone https://github.com/lisaac/luci-lib-docker package/diy/luci-lib-docker
+ rm -rf package/ipk/luci-app-dockerman && rm -rf package/diy/luci-app-dockerman && \
+ git clone https://github.com/lisaac/luci-app-dockerman package/diy/luci-app-dockerman
+ for i in `find package/diy/luci-app-dockerman/applications/luci-app-dockerman/`; do
+[ `grep -c "admin" $i 2>/dev/null` -gt "0" ] && sed -e 's|admin/docker|admin/services/docker|g; s|admin", "docker|admin", "services", "docker|g; s|admin","docker|admin", "services", "docker|g; s|admin\\/docker|admin\\/services\\/docker|g' $i -i
+done
 rm -rf package/ipk/luci-lib-docker
+rm -rf package/diy/luci-lib-docker && git clone https://github.com/lisaac/luci-lib-docker package/diy/luci-lib-docker
 rm -rf feeds/packages/utils/ttyd && \
 svn co https://github.com/coolsnowwolf/packages/trunk/utils/ttyd package/ipk/ttyd
 rm -rf package/network/services/ppp && \
