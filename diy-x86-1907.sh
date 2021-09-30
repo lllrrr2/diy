787548fc90a7331b16307e8164e46f02f454693a
hong0980 <hong0980@qq.com>
2021-09-30 12:09:11 +0800

    update@2021-09-30-12-09

--- diy-x86-1907.sh
+++ diy-x86-1907.sh
@@ -74,7 +74,7 @@ rm -rf package/lean/luci-app-qbittorrent
 #rm -rf package/lean/qBittorrent #4.3.4.1
 rm -rf diy/hong0980/qbittorrent #4.2.5
 rm -rf diy/hong0980/qt5 #5.98
-#sed -i 's/+qbittorrent/+qBittorrent-Enhanced-Edition/g' package/ipk/luci-app-qbittorrent/Makefile #qBittorrent-Enhanced-Edition 4.2.3.10
+sed -i 's/+qBittorrent/+qBittorrent-Enhanced-Edition/g' package/ipk/luci-app-qbittorrent/Makefile #qBittorrent-Enhanced-Edition 4.2.3.10
 #sed -i 's/+qBittorrent/+qBittorrent +python3/g' package/ipk/luci-app-qbittorrent/Makefile
 
 echo '替换aria2'
