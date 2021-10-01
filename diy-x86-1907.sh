87a48b5f70fb530a9d90122330444310e93f6fa8
hong0980 <hong0980@qq.com>
2021-10-01 15:41:20 +0800

    Update diy-x86-1907.sh

--- diy-x86-1907.sh
+++ diy-x86-1907.sh
@@ -76,7 +76,7 @@ rm -rf diy/hong0980/qt5 #5.98
 # svn co https://github.com/immortalwrt/packages/trunk/libs/qt5 feeds/packages/libs/qt5
 svn co https://github.com/immortalwrt/packages/trunk/libs/libtorrent-rasterbar feeds/packages/libs/libtorrent-rasterbar
 svn co https://github.com/immortalwrt/packages/trunk/net/qBittorrent-Enhanced-Edition feeds/packages/net/qBittorrent-Enhanced-Edition
-sed -i 's/+qBittorrent/+qBittorrent-Enhanced-Edition/g' package/ipk/luci-app-qbittorrent/Makefile #qBittorrent-Enhanced-Edition 4.2.3.10
+#sed -i 's/+qBittorrent/+qBittorrent-Enhanced-Edition/g' package/ipk/luci-app-qbittorrent/Makefile #qBittorrent-Enhanced-Edition 4.2.3.10
 #sed -i 's/+qBittorrent/+qBittorrent +python3/g' package/ipk/luci-app-qbittorrent/Makefile
 
 echo '替换aria2'
