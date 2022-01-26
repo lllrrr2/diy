004537be3a2727543b00790f15e421caf5c14a62
hong0980 <hong0980@qq.com>
2022-01-27 00:43:25 +0800

    Update lean_openwrt.sh

--- lean_openwrt.sh
+++ lean_openwrt.sh
@@ -332,7 +332,7 @@ case $TARGET in
 	_packages "
 	luci-app-adbyby-plus
 	luci-app-adguardhome
-	luci-app-amule
+	#luci-app-amule
 	luci-app-dockerman
 	luci-app-netdata
 	luci-app-openclash
@@ -360,7 +360,7 @@ case $TARGET in
 	luci-app-passwall_INCLUDE_V2ray=y
 	luci-app-passwall_INCLUDE_V2ray_Plugin=y
 	luci-app-passwall_INCLUDE_Xray=y
-	AmuleWebUI-Reloaded ariang bash htop lscpu lsscsi lsusb nano pciutils screen webui-aria2 zstd tar pv
+	#AmuleWebUI-Reloaded ariang bash htop lscpu lsscsi lsusb nano pciutils screen webui-aria2 zstd tar pv
 	#subversion-server #unixodbc #git-http
 
 	#USB3.0支持
