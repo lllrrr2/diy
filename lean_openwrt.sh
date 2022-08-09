0a8ac737630dd53d9e3ba71de90b49defad6f614
hong0980 <hong0980@qq.com>
2022-08-09 14:53:50 +0800

    update@2022-08-09-14-53

--- lean_openwrt.sh
+++ lean_openwrt.sh
@@ -443,32 +443,14 @@ case $TARGET_DEVICE in
 		# wget -qP patches/ https://raw.githubusercontent.com/mingxiaoyu/R1-Plus-LTS/main/patches/0001-Add-pwm-fan.sh.patch && \
 		# git apply --reject --ignore-whitespace patches/*.patch
 	# fi
+	svn export --force https://github.com/friendlyarm/friendlywrt/trunk/target/linux/rockchip/armv8/base-files/etc/modules.d target/linux/rockchip/armv8/base-files/etc/modules.d
 	sed -i -e 's,kmod-r8168,kmod-r8169,g' target/linux/rockchip/image/armv8.mk
 	echo '
-	CONFIG_ARM64_CRYPTO=y
-	CONFIG_CRYPTO_AES_ARM64=y
-	CONFIG_CRYPTO_AES_ARM64_BS=y
-	CONFIG_CRYPTO_AES_ARM64_CE=y
-	CONFIG_CRYPTO_AES_ARM64_CE_BLK=y
-	CONFIG_CRYPTO_AES_ARM64_CE_CCM=y
-	CONFIG_CRYPTO_CRCT10DIF_ARM64_CE=y
-	CONFIG_CRYPTO_AES_ARM64_NEON_BLK=y
-	CONFIG_CRYPTO_CRYPTD=y
-	CONFIG_CRYPTO_GF128MUL=y
-	CONFIG_CRYPTO_GHASH_ARM64_CE=y
-	CONFIG_CRYPTO_SHA1=y
-	CONFIG_CRYPTO_SHA1_ARM64_CE=y
-	CONFIG_CRYPTO_SHA256_ARM64=y
-	CONFIG_CRYPTO_SHA2_ARM64_CE=y
-	CONFIG_CRYPTO_SHA512_ARM64=y
-	CONFIG_CRYPTO_SIMD=y
-	CONFIG_REALTEK_PHY=y
 	CONFIG_CPU_FREQ_GOV_USERSPACE=y
 	CONFIG_CPU_FREQ_GOV_ONDEMAND=y
 	CONFIG_CPU_FREQ_GOV_CONSERVATIVE=y
-	CONFIG_MOTORCOMM_PHY=y
 	CONFIG_SENSORS_PWM_FAN=y
-	' | tee -a target/linux/rockchip/armv8/{config-5.10,config-5.18} >/dev/null
+	' | tee -a target/linux/rockchip/armv8/{config-5.10,config-5.15,config-5.18,config-5.19,config-5.4} >/dev/null
 	;;
 "asus_rt-n16")
 	DEVICE_NAME="Asus-RT-N16"
