ec053c938c6ecdd8929c0018f7ec8b13d68d5f18
hong0980 <hong0980@qq.com>
2024-04-24 18:41:02 +0800

    update@2024-04-24-18-41

--- immortalwrt_openwrt.sh
+++ immortalwrt_openwrt.sh
@@ -558,7 +558,7 @@ esac
     [[ $TARGET_DEVICE =~ ^r ]] && \
     sed -i "s|VERSION.*|VERSION-5.4 = .273|; s|HASH.*|HASH-5.4.273 = 8ba0cfd3faa7222542b30791def49f426d7b50a07217366ead655a5687534743|" include/kernel-5.4
     clone_dir openwrt-23.05 immortalwrt/immortalwrt openssl
-    clone_dir openwrt-23.05 immortalwrt/packages nginx-util samba4 ttyd
+    clone_dir openwrt-23.05 immortalwrt/packages nginx-util samba4
     # clone_dir sbwml/openwrt_helloworld shadowsocks-rust chinadns-ng
     # clone_dir immortalwrt/packages nghttp3 ngtcp2 bash
     # clone_dir coolsnowwolf/lede opkg iproute2 hostapd ucode #uhttpd dnsmasq iwinfo
